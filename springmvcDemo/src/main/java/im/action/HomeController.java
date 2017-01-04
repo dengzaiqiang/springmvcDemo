6e09ae3b2b45788a575c06de41ca6cbf17d140cc
Administrator <279225212@qq.com>
2017-01-04 15:29:32 +0800

    test

--- springmvcDemo/src/main/java/im/action/HomeController.java
+++ springmvcDemo/src/main/java/im/action/HomeController.java
@@ -14,6 +14,7 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class HomeController implements Controller{
     public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        System.out.println("test!");
         return new ModelAndView("hello");
     }
 }
